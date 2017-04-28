307b3e1b148d8d3ba36100c7ee8e7c22422e7057
tmsa232 <tmsa232@uky.edu>
2017-04-27 21:13:56 -0400

    TextBubble Color

--- app/src/main/java/com/parley/parley/Settings.java
+++ app/src/main/java/com/parley/parley/Settings.java
@@ -28,20 +28,28 @@ public class Settings extends AppCompatActivity {
         //opens up the settings activity page if settingsButton is clicked
         final Button fontCustomize = (Button) findViewById(R.id.customize_font);
 
-        fontCustomize.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                startActivity(new Intent(Settings.this, FontSettingsActivity.class));
-            }
-        });
-
         //Defines buttons for the Text Bubble Shape RadioGroup
         final RadioButton textBubbleCircle = (RadioButton) findViewById(R.id.circle);
         final RadioButton textBubbleHexagon = (RadioButton) findViewById(R.id.hexagon);
         final RadioButton textBubbleQuote = (RadioButton) findViewById(R.id.quote_bubble);
         final RadioButton textBubbleTriangle = (RadioButton) findViewById(R.id.triangle);
         final RadioButton textBubbleStar = (RadioButton) findViewById(R.id.star);
+
+        fontCustomize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                SharedPreferences settings = getSharedPreferences("prefs", MODE_PRIVATE);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putBoolean("circle",textBubbleCircle.isChecked());
+                editor.putBoolean("hexagon",textBubbleHexagon.isChecked());
+                editor.putBoolean("quote",textBubbleQuote.isChecked());
+                editor.putBoolean("triangle",textBubbleTriangle.isChecked());
+                editor.putBoolean("star", textBubbleStar.isChecked());
+
+                startActivity(new Intent(Settings.this, FontSettingsActivity.class));
+            }
+        });
 
 
         final SharedPreferences settings = getSharedPreferences("answers", MODE_PRIVATE);
